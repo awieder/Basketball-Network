25fa73d085ae71f2341631d26359912dddd5823e
dhilon <70111176+dhilon@users.noreply.github.com>
2024-10-15 16:59:47 -0600

    delete again

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas
+
 from basketball_reference_scraper.basketball_reference_scraper.teams import get_roster, get_team_stats, get_opp_stats, get_roster_stats, get_team_misc
 
 print(get_team_misc('MEM', 2023, 'TOTALS'))
